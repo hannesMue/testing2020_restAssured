68953e6618e6f0df08fb2e7ef64b34eb0dafdd44
carol <carol@DESKTOP-DN7AVTB.fritz.box>
2020-05-20 13:55:38 +0200

    Update TestFunktion

--- RestAssured_Quickstart/src/main/java/quickstart/TestFunktion.java
+++ RestAssured_Quickstart/src/main/java/quickstart/TestFunktion.java
@@ -2,13 +2,19 @@ package quickstart;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import io.restassured.http.ContentType;
 
 
-public class TestFunktion {
+public class TestFunktion {	
+	
 	@Test
 	public void getAllEmployees() {
 		baseURI = "https://jsonplaceholder.typicode.com";
@@ -16,33 +22,21 @@ public class TestFunktion {
 		get("/todos").
 		then().log().body();
 	}
-	
-	@Test 
-	public void getEmployeeById() {
-		baseURI = "https://jsonplaceholder.typicode.com";
-		given().when().
-		get("/todos/1").
-		then().assertThat().statusCode(200).log().body();
-	}
-	
+
 	@Test 
 	public void createEmployee() {
+		
+		//Map um JSON-Objekt zu simulieren
+		Map<String, Object>  request = new HashMap();
+		request.put("userId","1");
+		request.put("id","100");
+		request.put("title","servus");
+		request.put("body","blablabla");
+		
 		baseURI = "https://jsonplaceholder.typicode.com";
-		given().params("userId","1","id","100","title","servus","completed","true").
+		given().body(request).
 		when().
-		post("/todos").
-		then().log().body();
+		post("/posts").
+		then().assertThat().statusCode(201);
 	}
-	
-	@Test
-	public void updateEmployee() {
-		baseURI = "https://jsonplaceholder.typicode.com";
-		given().params("name","test","salary","500","age","64").
-		when().
-		put("/todos").
-		then().assertThat().statusCode(200).
-		log().body();
-
-	}
-
 }
